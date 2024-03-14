2b53e33a2cedb4229dd29a90f3389a32d7ba1fa1
seungjjun <il1uso@naver.com>
2024-03-14 10:41:56 +0900

    add sub-module and workflow dispatch options

---
+++ sample-module/src/main/java/com/sample/SampleModuleApplication.java
@@ -0,0 +1,13 @@
+package com.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleModuleApplication.class, args);
+    }
+
+}
